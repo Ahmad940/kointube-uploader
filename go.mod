34bf5658389e6b9adcf3554f30ed2de747a91484
ahmad940 <ahmadmuhammadmak5@gmail.com>
2021-10-20 12:17:32 +0100

    initial repo

---
+++ go.mod
@@ -0,0 +1,22 @@
+module kointube
+
+go 1.17
+
+require (
+	github.com/cloudinary/cloudinary-go v1.3.0
+	github.com/gofiber/fiber/v2 v2.20.2
+	github.com/joho/godotenv v1.4.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.2 // indirect
+	github.com/creasty/defaults v1.5.1 // indirect
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/gorilla/schema v1.2.0 // indirect
+	github.com/klauspost/compress v1.13.4 // indirect
+	github.com/matoous/go-nanoid/v2 v2.0.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.29.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
+)
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/cloudinary/cloudinary-go"
+	"github.com/cloudinary/cloudinary-go/api/uploader"
+	"github.com/gofiber/fiber/v2"
+	_ "github.com/joho/godotenv/autoload"
+	gonanoid "github.com/matoous/go-nanoid/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	var cld, err = cloudinary.New()
+	if err != nil {
+		log.Fatalf("Failed to intialize Cloudinary, %v", err)
+	}
+
+	ctx := context.Background()
+
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("Hello World")
+	})
+
+	// Routes
+	app.Post("/upload/video", func(c *fiber.Ctx) error {
+		// Get first file from form field "document":
+		file, err := c.FormFile("video")
+		if err != nil {
+			return err
+		}
+
+		tempFile, err := file.Open()
+		if err != nil {
+			return err
+		}
+
+		id, err := gonanoid.New()
+		if err != nil {
+			return err
+		}
+
+		uploadResult, err := cld.Upload.Upload(
+			ctx,
+			tempFile,
+			uploader.UploadParams{PublicID: id, Folder: "kointube/video"})
+		if err != nil {
+			log.Fatalf("Failed to upload file, %v\n", err)
+		}
+
+		return c.JSON(map[string]string{
+			"SecureURL": uploadResult.SecureURL,
+		})
+	})
+
+	// Routes
+	app.Post("/upload/image", func(c *fiber.Ctx) error {
+		// Get first file from form field "document":
+		file, err := c.FormFile("image")
+		if err != nil {
+			return err
+		}
+
+		tempFile, err := file.Open()
+		if err != nil {
+			return err
+		}
+
+		id, err := gonanoid.New()
+		if err != nil {
+			return err
+		}
+
+		uploadResult, err := cld.Upload.Upload(
+			ctx,
+			tempFile,
+			uploader.UploadParams{PublicID: id, Folder: "kointube/image"})
+		if err != nil {
+			log.Fatalf("Failed to upload file, %v\n", err)
+		}
+
+		return c.JSON(map[string]string{
+			"SecureURL": uploadResult.SecureURL,
+		})
+	})
+
+	log.Fatal(app.Listen(os.Getenv("PORT")))
+}
